e06dd9325a10daca1afa1a59697ee023cdd5683a
w00ing <kimwy1997@gmail.com>
2020-07-23 11:47:46 +0900

    Import RoomType to room admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from . import models
 
-# Register your models here.
+
+@admin.register(models.RoomType)
+class ItemAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(models.Room)
