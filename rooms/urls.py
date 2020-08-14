2f59831672e0bf9a444698a2c674a2e11661f1ba
Woo-yeong Kim <kimwy1997@gmail.com>
2020-08-15 02:33:50 +0900

    Make Home view and Roomdetail view

---
+++ rooms/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rooms import views as room_views
+
+app_name = "rooms"
+
+urlpatterns = [
+    path("<int:pk>", room_views.RoomDetail.as_view(), name="detail"),
+]
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
+from . import models
 
-# Create your views here.
+
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+
+class RoomDetail(DetailView):
+
+    """ Room Detail Definition """
+
+    model = models.Room
+    # context_object_name = "room"
+
